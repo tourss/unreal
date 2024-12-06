32acdee94a01eb9213c344ba62830b72e61c0104
hyo <dlgyrl5759@gmail.com>
2024-12-06 10:48:19 +0900

    arange files

--- renderFarm/RemoteRenderSubmitter.py
+++
@@ -1,9 +0,0 @@
-import unreal
- 
-@unreal.uclass()
-class MyCustomRemoteRenderSubmitter(unreal.MoviePipelineExecutorBase):
- 
-    # A MoviePipelineExecutor implementation must override this.
-    @unreal.ufunction(override=True)
-    def execute(self, pipeline_queue):
-      unreal.log("Execute!")
